0ed64c43b5c7bb2ef08bf4e886e0fb38939f0814
hugh <huguanghui1971@gmail.com>
2023-06-25 08:43:43 +0800

    feat 完成异步线程方式调试

--- 01_QThread/main.py
+++ 01_QThread/main.py
@@ -3,6 +3,7 @@
 
 import sys
 import os
+import time
 
 if hasattr(sys, "frozen"):
     os.environ["PATH"] = sys._MEIPASS + ";" + os.environ["PATH"]
@@ -10,6 +11,39 @@ if hasattr(sys, "frozen"):
 from GUI.Ui_Main import Ui_MainWindow
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QStatusBar
+from PyQt5.QtCore import QThread, pyqtSignal, Qt, QMutex
+
+qmut_1 = QMutex()
+# qmut_2 = QMutex()
+
+
+class Thread_1(QThread):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        qmut_1.lock()
+        values = [1, 2, 3, 4, 5]
+        for i in values:
+            print(i)
+            time.sleep(0.5)
+        qmut_1.unlock()
+
+
+class Thread_2(QThread):
+    _signal = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        # qmut_2.lock()
+        values = ["a", "b", "c", "d", "e"]
+        for i in values:
+            print(i)
+            time.sleep(0.5)
+        # qmut_2.unlock()
+        self._signal.emit()
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -21,9 +55,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setWindowTitle("Test V1.0.0")
         # self.setWindowIcon(QIcon(":/app.ico"))
         self.setStatusBar(self.status_bar)
+        self.pb_1.clicked.connect(self.click_1)
+        self.pb_2.clicked.connect(self.click_2)
 
     def ui_init(self):
         print("ui_init")
+
+    def click_1(self):
+        self.thread_1 = Thread_1()
+        self.thread_1.start()
+
+    def click_2(self):
+        self.pb_2.setEnabled(False)
+        self.thread_2 = Thread_2()
+        self.thread_2._signal.connect(self.set_btn)
+        self.thread_2.start()
+
+    def set_btn(self):
+        self.pb_2.setEnabled(True)
 
 
 if __name__ == "__main__":
