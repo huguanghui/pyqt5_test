0fa5cedbc527bf1b3c115f741a09d8b02cf11797
huguanghui <522146829@qq.com>
2023-08-31 13:12:00 +0800

    fix 优化初始化

--- app/PTool.py
+++ app/PTool.py
@@ -1,11 +1,9 @@
 # coding:utf-8
 from PyQt5.QtCore import QLocale, Qt, QTranslator
 from PyQt5.QtWidgets import QApplication
-from qfluentwidgets import (NavigationItemPosition, NavigationAvatarWidget, SplitFluentWindow, FluentTranslator)
-from qfluentwidgets import FluentIcon as FIF
 
 from common.application import SingletonApplication
-# from common.config import config, Language
+from common.config import config, Language
 from common.setting import APP_NAME
 # from common.dpi_manager import DPI_SCALE
 from View.main_window import MainWindow
@@ -25,54 +23,6 @@ os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
 # os.environ["QT_SCALE_FACTOR"] = str(DPI_SCALE)
 
 
-# class Window(SplitFluentWindow):
-#     def __init__(self):
-#         super().__init__()
-
-#         # create sub interface
-#         self.page01 = Page01Test(self)
-#         self.page02 = Page02Test(self)
-#         self.settingInterface = SettingInterface(self)
-
-#         self.initNavigation()
-
-#     def initNavigation(self):
-#         # add sub interface
-#         self.addSubInterface(self.page01, FIF.RINGER, '专注时段')
-#         self.addSubInterface(self.page02, FIF.CODE, '流播放')
-#         # self.addSubInterface(self.stopWatchInterface, FIF.STOP_WATCH, '秒表')
-
-#         # self.navigationInterface.addWidget(
-#         #     routeKey='avatar',
-#         #     widget=NavigationAvatarWidget('zhiyiYo', 'resource/images/shoko.png'),
-#         #     onClick=self.showMessageBox,
-#         #     position=NavigationItemPosition.BOTTOM,
-#         # )
-#         self.navigationInterface.addItem(
-#             routeKey='settingInterface',
-#             icon=FIF.SETTING,
-#             text='设置',
-#             position=NavigationItemPosition.BOTTOM,
-#         )
-
-#         self.navigationInterface.setExpandWidth(280)
-
-#     def switchTo(self, interface):
-#         print(interface)
-#         self.stackedWidget.setCurrentWidget(interface, popOut=False)
-#         # 实现切换界面的逻辑
-#         if interface == self.page01:
-#             print("Switching to page01")
-#         elif interface == self.page02:
-#             print("Switching to page02")
-#             m_w = self.page02.videostream.geometry().width()
-#             m_h = self.page02.videostream.geometry().height()
-#             print(m_w, m_h)
-#             print(self.page02.width(), self.page02.height())
-
-#     def showMessageBox(self):
-#         print('showMessageBox')
-
 if __name__ == "__main__":
     QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
@@ -80,26 +30,14 @@ if __name__ == "__main__":
 
     app = QApplication(sys.argv)
     app.setApplicationName(APP_NAME)
-    # translator = FluentTranslator()
     translator = QTranslator()
-    translator.load(f":/i18n/ptool.zh_CN.qm")
+    language = config.get(config.language)  # type: Language
+    print("language:", QLocale.system().name())
+    if language == Language.AUTO:
+        translator.load(QLocale.system(), ":/i18n/ptool.")
+    elif language != Language.ENGLISH:
+        translator.load(f":/i18n/ptools.{language.value}.qm")
     app.installTranslator(translator)
     w = MainWindow()
     w.show()
     sys.exit(app.exec_())
-
-# app = SingletonApplication(sys.argv, APP_NAME)
-# app.setAttribute(Qt.AA_DontCreateNativeWidgetSiblings)
-# app.setApplicationName(APP_NAME)
-
-# Internationalization
-# translator = QTranslator()
-# language = config.get(config.language)  # type: Language
-
-# if language == Language.AUTO:
-#     translator.load(QLocale.system(), ":/i18n/Groove_")
-# elif language != Language.ENGLISH:
-#     translator.load(f":/i18n/ptools_{language.value}.qm")
-
-# app.installTranslator(translator)
-
