b7e1a4a868f43e3caf287d63c13bb86d44d14c24
huguanghui <huguanghui1971@gmail.com>
2023-10-30 09:19:03 +0800

    feat 完善帮助文档

--- app_template/View/help_widget/help_widget.py
+++ app_template/View/help_widget/help_widget.py
@@ -1,27 +1,16 @@
 # coding:utf-8
 
-from PyQt5.QtCore import Qt, pyqtSignal, QOperatingSystemVersion
-from PyQt5.QtWidgets import QLabel, QFrame, QHBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget
 
 from .ui_help import Ui_Help
 
-class SettingInterface(QFrame):
-    """ Setting interface """
 
-    checkUpdateSig = pyqtSignal()
-    crawlMetaDataFinished = pyqtSignal()
-    musicFoldersChanged = pyqtSignal(list)
-    acrylicEnableChanged = pyqtSignal(bool)
-    downloadFolderChanged = pyqtSignal(str)
-    minimizeToTrayChanged = pyqtSignal(bool)
-
+class HelpDoc(Ui_Help, QWidget):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-        self.hBoxLayout = QHBoxLayout(self)
+        self.setupUi(self)
+        self.ui_init()
 
-        self.label.setAlignment(Qt.AlignCenter)
-        self.hBoxLayout.addWidget(self.label, 1, Qt.AlignCenter)
-        self.setObjectName("settings")
-        # setting label
-        # self.settingLabel = QLabel(self.tr("Settings"), self)
-        # self.settingLabel.setAlignment(Qt.AlignCenter)
+    def ui_init(self):
+        print("ui_init")
